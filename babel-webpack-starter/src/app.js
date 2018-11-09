d66fc420de8467b357283e98d60a87f188924ed8
Rokas Grigaravicius <grigroka@gmail.com>
2018-11-09 11:48:14 +0200

    babel-webpack-starter

---
+++ babel-webpack-starter/src/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import { person, sayHello } from "./lib";
+
+console.log(person.name);
+console.log(sayHello('Rokas'));
+
+async function getPost() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await response.json();
+    return data;
+}
+
+getPost().then(posts => console.log(posts));
